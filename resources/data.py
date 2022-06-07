efd827f57e0edb40399e40472e428c80d606f588
S P A R K Y <100108568+SPARKY-00@users.noreply.github.com>
2022-06-07 15:14:14 +0530

    Update data.py

--- resources/data.py
+++ resources/data.py
@@ -1,7 +1,7 @@
-GRP = "@DNHxHELL|@RiZoeLXSpam|@ArrayCoreChats|DNHxHELL|RiZoeLXSpam|ArrayCoreChats"
+GRP = "@DNHxHELL|@XTron|@ArrayCoreChats|DNHxHELL|XTron|ArrayCoreChats"
 GROUP = [-1001321613309, -1001771478102, -1001749467927]
 RiZoeLX = [1517994352, 2086101519, 1854700253]
-TheRiZoeL = "@TheRiZoeL|@TheVenomxd|RiZoeL"
+TheRiZoeL = "@TheRiZoeL|@TheVenomxd|Tron"
 REPLYRAID = [
     "𝙈𝘼𝘿𝘼𝙍𝘾𝙃𝙊𝘿",
     "𝘽𝙃𝙊𝙎𝘿𝙄𝙆𝙀",
